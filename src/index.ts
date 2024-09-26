7d4cdb77aa92fc2dabd4547e4fc9a4396679c9a2
GaruVA <garukaassalaarachchi@gmail.com>
2024-09-26 13:48:29 +0530

    Add Readme, Move Connection to a seperate file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/database';
 
 dotenv.config();
 
@@ -11,9 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI as string)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('GarbageTrack API is running');
